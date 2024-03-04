66195ded59e09828e22f2feab4f0ec39d6e42560
carlalap <carlalap>
2024-03-04 11:22:25 -0600

    task1. Root Query for a particular task addedm, app.js updated

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema'); // check path file
 
 const app = express();
 
+// add schema in an object we pass to the graphqlHTTP()
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true, // Enables GraphiQL, a graphical interactive in-browser GraphQL IDE
+}));
+
+// Listening for requests on port 4000 (update Dockerfile)
 app.use('/graphql', graphqlHTTP({
 }));
-app.listen(8080, () => {
-  console.log('now listening for request on port 8080\n');
+app.listen(4000, () => {
+  console.log('now listening for request on port 4000\n');
 });
--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,5 +1,8 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+// Script that contains the schema property.
 
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+
+// Creates GraphQLObjectType: TaskType which contains 2 parameters (Tawsk, fields)
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
@@ -10,4 +13,32 @@ const TaskType = new GraphQLObjectType({
   },
 });
 
-module.exports = { TaskType };
+/* Creates type that represents all of the possible entry points into the GraphQL API,
+ * create a root query to query for a particular task
+*/
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        // Code to retrieve data from the database based on args.id
+        // This function should return the task object with the provided id
+        // For now, let's return a hardcoded task object
+        return {
+          id: args.id,
+          title: 'Sample Task',
+          weight: 5,
+          description: 'This is a sample task',
+        };
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQueryType
+});
