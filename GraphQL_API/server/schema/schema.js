0c14ac6d9964d0d50b9c4dbc1471b38fdf05f991
carlalap <carlalap>
2024-03-04 12:35:57 -0600

    task2. testting query in graphiql added ok

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,12 +1,20 @@
 // Script that contains the schema property.
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID } = require('graphql');
+const _ = require('lodash');
+
+// taks2. dummy data to be used to be used in the resolve function
+const tasks = [
+  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)' },
+  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order' }
+];
+
 
 // Creates GraphQLObjectType: TaskType which contains 2 parameters (Tawsk, fields)
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
@@ -21,24 +29,14 @@ const RootQueryType = new GraphQLObjectType({
   fields: {
     task: {
       type: TaskType,
-      args: {
-        id: { type: GraphQLString }
-      },
+      args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        // Code to retrieve data from the database based on args.id
-        // This function should return the task object with the provided id
-        // For now, let's return a hardcoded task object
-        return {
-          id: args.id,
-          title: 'Sample Task',
-          weight: 5,
-          description: 'This is a sample task',
-        };
+        return _.find(tasks, { id: args.id });
       },
     },
   },
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQueryType
+  query: RootQueryType,
 });
