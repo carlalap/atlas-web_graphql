8fd8e33b4d7e886ce677f1bd064ea57e99907c56
carlalap <carlalap>
2024-03-09 20:21:58 -0600

    test

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -1,29 +1,33 @@
-/*import ApolloClient from 'apollo-boost';
-/*
-...
+/* task.9 Add the front-end part and setup Apollo Client
+* import ApolloClient from 'apollo-boost';
 */
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 // components
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
+
 // apollo client setup
-/*
-...
-*/
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
-  );
+  return (
+    // Wrap the App component with the ApolloProvider and pass the client
+    <ApolloProvider client={client}>
+      <div id="main">
+        <div id = "bg" > </div> 
+        <h1> Holberton school tasks </h1> 
+        <TaskList/>
+        <AddProject/>
+        <AddTask/>
+      </div>
+    </ApolloProvider>
+   );
 }
 
 export default App;
--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -2,9 +2,13 @@ import {
   useState,
   //useEffect
 } from "react";
+import { graphql } from "react-apollo";
+import { flowRight as compose } from "lodash";
+import { getProjectQuery, addTaskMutation, getTaskQuery } from "../queries/queries";
 
 
 function AddTask(props) {
+
   const [inputs, setInputs] = useState({
     title: '',
     weight: 1,
@@ -12,85 +16,76 @@ function AddTask(props) {
     projectId: ''
   });
 
+  function displayProjects() {
+    //  console.log(props);
+    const data = props.getProjectQuery;
+    if (data.loading) {
+      return (<option disabled> Loading projects... </option>);
+      } else {
+        return data.projects.map(project => {
+            return ( 
+            < option 
+              key = { project.id }
+              value = { project.id } 
+            >  { project.title } 
+            </option>
+              );
+            })
+        }
+      }
 
-  const handleChange = (e) => {
-        const newInputs = {
-          ...inputs
-        };
-        if (e.target.name === "weight") newInputs[e.target.name] = parseInt(e.target.value)
-        else newInputs[e.target.name] = e.target.value
-        setInputs(newInputs)
-  }
+      const handleChange = (e) => {
+            const newInputs = {
+              ...inputs
+            };
+            if (e.target.name === "weight") newInputs[e.target.name] = parseInt(e.target.value)
+            else newInputs[e.target.name] = e.target.value
+            setInputs(newInputs)
+      }
 
-  return ( <
-    form class = "task"
-    id = "add-task"
-    /*onSubmit = {...}*/ >
-    <
-    div className = "field" >
-    <
-    label > Task title: < /label> <
-    input type = "text"
-    name = "title"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.title
-    }
-    required /
-    >
-    < /
-    div > <
-    div className = "field" >
-    <
-    label > Weight: < /label> <
-    input type = "number"
-    name = "weight"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.weight
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > description: < /label> <
-    textarea name = "description"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.description
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > Project: < /label> <
-    select name = "projectId"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.projectId
-    }
-    required > < option value = ""
-    selected = "selected"
-    disabled = "disabled" > Select project < /option>  < /
-    select > < /
-    div >
-    <
-    button > + < /button> < /
-    form >
+  return (
+    <form class = "task" id = "add-task" onSubmit = {}>
+        <div className = "field">
+          <label> Task title: </label> 
+        <input 
+          type = "text"
+          name = "title"
+          onChange = { handleChange }
+          value = { inputs.title }
+          required />
+        </div > 
+        <div className = "field">
+          <label> Weight: </label> 
+          <input 
+            type = "number"
+            name = "weight"
+            onChange = { handleChange }
+            value = { inputs.weight }
+            required />
+        </div >
+        <div div className = "field" >
+          <label> description: </label> 
+            <textarea 
+              name = "description"
+              onChange = { handleChange }
+              value = { inputs.description }
+              required />
+        </div >
+        <div className = "field" >
+        <label> Project: </label> 
+        <select 
+          name = "projectId"
+          onChange = { handleChange }
+          value = { inputs.projectId }
+          required> 
+          <option 
+            value = ""
+            selected = "selected"
+            disabled = "disabled"> Select project </option>  
+            </select> 
+            </div>
+        <button> + </button> 
+    </form >
   );
 }
 
